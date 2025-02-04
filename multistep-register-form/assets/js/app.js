7b841fb76b201e7a10b7841e08fd6af03ac74210
aguirre-ivan <iaguirre@fi.uba.ar>
2025-02-04 14:42:59 -0300

    Added dot navigation

--- multistep-register-form/assets/js/app.js
+++ multistep-register-form/assets/js/app.js
@@ -1,6 +1,7 @@
 const steps = document.querySelectorAll(".register-form__step");
 const nextButtons = document.querySelectorAll(".register-form__button--next");
 const dots = document.querySelectorAll(".form-pagination__dot");
+const stepInfo = document.querySelector(".form-pagination__step");
 
 document.addEventListener("DOMContentLoaded", function () {
     let currentStep = 0;
@@ -19,6 +20,8 @@ document.addEventListener("DOMContentLoaded", function () {
             dot.classList.toggle("form-pagination__dot--filled", fill_dot);
             dot.classList.toggle("form-pagination__dot--current", i === index);
         });
+
+        stepInfo.textContent = `Step ${index + 1} of ${steps.length}`;
     }
     
     function nextStep() {
@@ -28,7 +31,23 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     }
     
+    function goToStep(index) {
+        let selectedDot = dots[index];
+        if (selectedDot.classList.contains("form-pagination__dot--filled")) {
+            currentStep = index;
+            showStep(currentStep);
+        }
+    }
+
+    dots.forEach((dot, i) => {
+        dot.addEventListener("click", () => {
+            goToStep(i);
+        });
+    });
+
     nextButtons.forEach((button) => {
         button.addEventListener("click", nextStep);
     });
+
+    showStep(currentStep);
 });

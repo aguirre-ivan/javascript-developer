98f9cb8c6319d265661e98aa3007aab00fe6ec5b
aguirre-ivan <iaguirre@fi.uba.ar>
2025-02-08 13:54:46 -0300

    Added base url2

--- qr-code-generator/assets/js/app.js
+++ qr-code-generator/assets/js/app.js
@@ -1,3 +1,12 @@
+const qr_base = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=chau"
+const qr = document.querySelector(".qr-generator__qr img");
+const urlInput = document.querySelector(".qr-generator__url-input input");
+
+urlInput.addEventListener("input", (e) => {
+    qr.src = qr_base + e.target.value;
+});
+
+
 const form = document.querySelector(".qr-generator__url-input form");
 const mainContainer = document.querySelector(".main-container");
 
